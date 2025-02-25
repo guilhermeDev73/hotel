40c0a3d50a597d9f59b251ffafc73550ca1a26a6
guilhermenn33 <claraguilherme308@gmail.com>
2025-02-25 13:38:45 -0300

    Update comentarios.jsx

--- src/components/layout/comentarios.jsx
+++ src/components/layout/comentarios.jsx
@@ -20,7 +20,7 @@ export default function Comentarios() {
             <h1 className="font-medium text-3xl">Comentarios</h1>
             <div className="relative flex w-full max-w-[700px] justify-center hover:scale-105 transition">
                 <FaChevronLeft className="absolute left-0 top-[50%] text-2xl cursor-pointer hover:scale-110 transition" onClick={e => voltarComentario()} />
-                <section className="flex gap-24 overflow-hidden w-[90%] max-w-[400px] transition-all ease-in-out ">
+                <section className="flex gap-24 overflow-hidden w-[90%] max-w-[300px] transition-all ease-in-out ">
                     <div className="min-w-[350px] w-[100%] min-h-[350px] bg-white rounded-md mt-4 flex flex-col justify-center items-center text-black p-5">
                         <img src={Comentariosdata[comentarioNum].foto} alt="" className="w-[100px] h-[100px] object-cover rounded-[50%]" />
                         <h3 className="font-bold text-xl">{Comentariosdata[comentarioNum].nome}</h3>
