f2723b9e15cabc19193b2f3cc0cc66967af710f9
guilhermenn33 <claraguilherme308@gmail.com>
2025-02-25 13:34:29 -0300

    Update comentarios.jsx

--- src/components/layout/comentarios.jsx
+++ src/components/layout/comentarios.jsx
@@ -18,7 +18,7 @@ export default function Comentarios() {
         <section className="min-h-[60vh] w-3/3 bg-blue-950 flex flex-col items-center text-white  pt-2 overflow-hidden gap-3 relative">
 
             <h1 className="font-medium text-3xl">Comentarios</h1>
-            <div className="relative flex w-full max-w-[700px] justify-center hover:scale-110 transition">
+            <div className="relative flex w-full max-w-[700px] justify-center hover:scale-105 transition">
                 <FaChevronLeft className="absolute left-0 top-[50%] text-2xl cursor-pointer hover:scale-110 transition" onClick={e => voltarComentario()} />
                 <section className="flex gap-24 overflow-hidden w-[90%] max-w-[400px] transition-all ease-in-out ">
                     <div className="min-w-[350px] w-[100%] min-h-[350px] bg-white rounded-md mt-4 flex flex-col justify-center items-center text-black p-5">
